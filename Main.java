e0dec0d0742c11c706b346807898978cc23ab75f
Edson Junior <edson.junior@ufpi.edu.br>
2026-06-11 11:33:57 -0300

    Issue 001 e Issue 003

--- Main.java
+++ Main.java
@@ -3,8 +3,14 @@ import java.util.Arrays;
 public class Main {
 
 	public static void main(String[] args) {
-		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes"));
-		System.out.println(mainMenu.getSelection() + "foi selecionada");
+		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes"), true);
+		int selection = -1;
+		while (selection != 0) {
+			selection = mainMenu.getSelection();
+			if (selection != 0) {
+				System.out.println(selection + " foi selecionada");
+			}
+		}
 		System.out.println("Fim");
 	}
 
